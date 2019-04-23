f5f4606b8dfb4649261e8521accb0d02677fdef8
Edylson Manuel Goncalves Frederico <a39221@alunos.ipb.pt>
2019-04-23 12:16:01 +0100

    crud

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+
+<?php
+
+$Reserva = new Reserva();
+$Reserva->DataDeReserva = 22/11/19;
+$Reserva->DataDeDevolucao = 24/11/19;
+
+$res = $Reserva->create();
+if(!$res){
+    print "ERRO";
+}
+
+?>
+
+
+</body>
+</html>
+
+
