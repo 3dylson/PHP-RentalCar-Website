e4053132065bf0636166da5909b836702b03e0eb
Edylson Manuel Goncalves Frederico <a39221@alunos.ipb.pt>
2019-04-23 11:54:13 +0100

    php camadas

---
+++ BL/Reserva.php
@@ -0,0 +1,8 @@
+<?php
+
+
+class Reserva
+{
+    public $idReserva;
+    public $DataDeReserva;
+}
---
+++ DAL/ReservaDAL.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Apenas métodos estáticos
+ */
+
+class ReservaDAL
+{
+    public static function create(){
+        // Conexão à BD
+        // executar comando INSERT
+    }
+}
