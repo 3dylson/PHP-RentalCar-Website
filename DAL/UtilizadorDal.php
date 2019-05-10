49eb130d73afbcb9e4c242bbc625018c6fe004e8
Aritson Lima Melo <a36749@alunos.ipb.pt>
2019-05-10 17:39:20 +0100

    Adicionado php das paginas: - Categoria de veiculo; - Utilizador

--- DAL/UtilizadorDal.php
+++ DAL/UtilizadorDal.php
@@ -8,5 +8,52 @@
 
 class UtilizadorDal
 {
+    public static function create($Utilizador){
 
+        $db=DB::getInstance();
+
+        $query = "INSERT INTO Utilizador (nome, email, dataNascimento, password, passwordConfirmed, nif, admin) 
+VALUES (:nome, :email, :dataNascimento, :password, :passwordConfirmed, :nif, :admin)";
+        $res=$db->query($query, array(':nome'=> $Utilizador->nome,
+            'email'=> $Utilizador->email, 'dataNascimento'=> $Utilizador->dataNascimento, 'password'=> $Utilizador->password, 'passwordConfirmed'=> $Utilizador->passwordConfirmed, 'nif'=> $Utilizador->nif, 'admin'=> $Utilizador->admin));
+
+        if($res){
+            $Reserva->idReserva=$db->lastInsertId();
+        }
+        return $res;
+    }
+
+    public static function update($Utilizador){
+
+        $db=DB::getInstance();
+
+        $query = "UPDATE Utilizador SET VALUES (nome = :nome, email = :email, dataNascimento = :dataNascimento, password = :password, passwordConfirmed = :passwordConfirmed, nif = :nif, admin = :admin)";
+        $res=$db->query($query, array(':nome'=> $Utilizador->nome,
+            'email'=> $Utilizador->email, 'dataNascimento'=> $Utilizador->dataNascimento, 'password'=> $Utilizador->password, 'passwordConfirmed'=> $Utilizador->passwordConfirmed, 'nif'=> $Utilizador->nif, 'admin'=> $Utilizador->admin));
+
+        if($res){
+            $Utilizador->idUtilizador=$db->lastInsertId();
+        }
+        return $res;
+    }
+
+    public static function delete($id){
+
+        $db=DB::getInstance();
+
+        $query = "DELETE FROM Utilizador WHERE idUtilizador = ':idUtilizador'";
+        $res=$db->query($query, array(':idUtilizador'=> $id));
+
+        return $res;
+    }
+
+    public static function getAll(){
+
+        $db=DB::getInstance();
+
+        $query = "SELECT * FROM Utilizador";
+        $res=$db->query($query);
+        $res->setFetchMode( PDO::FETCH_CLASS, "Utilizador");
+        return $res;
+    }
 }
