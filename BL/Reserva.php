00db9e5ae0e68c44a9a9448d401a0d78e6c87a9f
Edylson Manuel Goncalves Frederico <a39221@alunos.ipb.pt>
2019-05-10 11:42:25 +0100

    Reserva BL Done.

--- BL/Reserva.php
+++ BL/Reserva.php
@@ -4,12 +4,44 @@
 class Reserva
 {
     public $idReserva;
-    public $DataDeReserva;
+    public $DataDaReserva;
     public $DataDeDevolucao;
+    public $LocalPickUp;
+    public $LocalDropOff;
+
+    public $Cliente_idCliente;
+    public $Promoção_idPromoção;
+
+    public function copy($objeto) {
+        foreach ($this as $key => $value) {
+            $this->$key=$objeto->$key;
+        }
+    }
 
     public function create(){
 
         $res = ReservaDAL::create($this);
         return($res);
     }
+
+    public function update() {
+        $res = ReservaDAL::update($this);
+        return($res);
+    }
+
+    public function delete() {
+        $res = ReservaDAL::delete($this);
+        return($res);
+    }
+
+    public function getAll() {
+        $res = ReservaDAL::getAll($this);
+        return($res);
+    }
+
+    public function getByData($DataDaReserva) {
+        $res = ReservaDAL::getByData($DataDaReserva);
+        return($res);
+    }
+
 }
