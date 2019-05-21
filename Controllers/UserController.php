396faf64b5d597e6e3174ea59abff6aebc9fc510
Edylson Manuel Goncalves Frederico <a39221@alunos.ipb.pt>
2019-05-21 23:51:41 +0100

    Progress.

--- Controllers/UserController.php
+++ Controllers/UserController.php
@@ -1,7 +1,139 @@
 <?php
-
+require_once dirname(__FILE__)."/../BL/User.php";
 
 class UserController
 {
+    public static function process(){
+        if(isset($_POST['user-register'])){
+            $nome_login=$_POST['nome_login'];
+            $nome=$_POST['nome'];
+            $email=$_POST['email'];
+            $dataNascimento=$_POST['dataNascimento'];
+            $password=$_POST['password'];
+            $passwordConfirmated=$_POST['passwordConfirmated'];
+            $nif=$_POST['nif'];
+            if(!$nome_login){
+                $msg["estado"]='Insira o seu Username.';
+            }elseif (!$nome) {
+                $msg["estado"]='Insira o Nome';
+            }elseif(!$email){
+                $msg["estado"]='Insira um Email.';
+            }elseif(!$dataNascimento){
+                $msg["estado"]='Insira a data do seu Nascimento.';
+            } elseif (!$password) {
+                $msg["estado"]='Insira uma password.';
+            }elseif (self::verificarnif($nif)) {
+                $msg["estado"]='O nif ja existe!';
+            }elseif(self::verificarlogin1($nome_login)){
+                $msg["estado"]='Já existe um usuario com este login!';
+            }
+            elseif(is_int($nome)){
+                $msg["estado"]='Insira um nome valido';
+            }elseif($password!=$passwordConfirmated){
+                $msg["estado"]='As password não coincidem!';
+            }else{
+                self::createUser();
+                $msg["estado"]='Cadastro Efectuado Com Sucesso!';
+            }
+            return  $msg["estado"];
+        }
 
+
+        if(isset($_POST['log'])){
+            self::verificarlogin();
+            if(isset($_SESSION['ID']) && $_SESSION['ID']>0){
+                header('Location: ./Home.php');
+                //unset($_GET['page']);
+            }
+        }
+
+
+
+    }
+
+
+    public static function createUser(){
+        $user= new User('',$_POST['nome_login'],$_POST['nome'],$_POST['email'],$_POST['dataNascimento'],$_POST['password'],$_POST['nif'],'');
+        if(isset($_SESSION['ID']) && UserController::typeofuser()){$user->usertype=1;}
+        else {$user->usertype=0;}
+        $user->create();
+    }
+
+    public static function createAdmin(){
+        $user= new User('',$_POST['nome_login'],$_POST['nome'],$_POST['email'],$_POST['dataNascimento'],$_POST['password'],$_POST['nif'],'');
+        $user->usertype='1';
+        $user->create();
+    }
+
+    public static function verificarnif($nifaux){
+        $user=new User();
+        $user->nif=$nifaux;
+        return $user->verificarnif();
+    }
+    public static function verificarlogin(){
+        $user1=new User();
+        $user1->login=$_POST['login1'];
+        $user1->password=$_POST['palavra_passe'];
+        if(($aux=$user1->verificarlogin())>0 || $aux==-1)
+            $_SESSION['ID']=$aux;
+    }
+    public static function verificarlogin1($logaux){
+        $user=new User();
+        $user->login=$logaux;
+        return $user->verificarlogin1();
+    }
+    public static function verificarPrimeiroUtilizador(){
+        return User::verificarPrimeiroUtilizador();
+    }
+    public static function getInformaCliente(){
+        return User::getInfofromUser();
+    }
+
+    public static function ProcessLogout(){
+        if(isset($_GET['terminarSessao'])){
+            $_SESSION = array();
+            if (ini_get("session.use_cookies")) {
+                $params = session_get_cookie_params();
+                setcookie(session_name(), '', time() - 42000,
+                    $params["path"], $params["domain"],
+                    $params["secure"], $params["httponly"]
+                );}
+            session_destroy();
+            //$_GET['page']='login';
+            header('Location: ./Home.php');
+        }
+        if(isset($_POST['changepass'])){
+            if(!$_POST['new_password']){
+                $msg["estado"]='Insira uma nova password.';
+            }elseif(self::verificarPass()!=$_POST['password_antigo']){
+                $msg["estado"]='Password antigo não coincide!';
+            }elseif($_POST['repeatNewPass']!=$_POST['new_password']){
+                $msg["estado"]='Os Passwords não coincidem';
+            }else{
+                self::alterarPass();
+                $msg["estado"]='Password alterado Com sucesso!';
+            }
+            return $msg["estado"];
+        }
+    }
+
+    public static function loginverification(){
+        if(isset($_SESSION['ID']))
+            return true;
+        else
+            return false;
+    }
+
+    public static function typeofuser(){
+        if(self::loginverification()){
+            $user= new User($_SESSION['ID']);
+            return $user->typeofuser();
+        }
+    }
+    static public function verificarPass(){
+        return User::verificarPass();
+    }
+    static public function alterarPass(){
+        User::alterarPass();
+    }
 }
--- index.php
+++ index.php
@@ -1,7 +1,21 @@
+<?php
+if(session_status()==PHP_SESSION_NONE){
+    session_start();
+}
+require_once 'Controllers/MainController.php';
+$msg=[
+    "estado"=>[],
+    "user"=>[]
+];
+$msg= MainController::process();
+if(isset($_SESSION['ID'])) $userinfo= MainController::getUserInformation();
+?>
+
+
 <!doctype html>
 <html lang="en">
 <head>
-    <title>CRUD</title>
+    <title>Rental Car - <?php echo MainController::title();?></title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -11,7 +25,50 @@
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
+<body class="bgHome">
+<div class="container">
+    <div class="row">
+        <div class="col-sm-6">
+            <a href="index.html">
+                <img id="logosize" src="../Imagens/Logo.png" height="115" width="218"/></a>
+        </div>
+        <div class="col-sm-6">
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav navbar-right">
+                    <br>
+                    <button class="open-button" onclick="openForm()"><span class="glyphicon glyphicon-log-in"></span> Login</button>
+                    <div class="form-popup" id="myForm">
+                        <form action="/action_page.php" class="form-container">
+                            <h1>Login</h1>
 
+                            <label for="email"><b>Email</b></label>
+                            <br>
+                            <input id="log2" type="email" placeholder="Enter Email" name="email" required>
+
+                            <label for="password"><b>Password</b></label>
+                            <input id="log" type="password" placeholder="Enter Password" name="psw" required>
+                            <br>
+                            <button type="submit" class="btn">Login</button>
+                            <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
+                        </form>
+                        <script>
+                            function openForm() {
+                                document.getElementById("myForm").style.display = "block";
+                            }
+
+                            function closeForm() {
+                                document.getElementById("myForm").style.display = "none";
+                            }
+                        </script>
+                    </div>
+                    <a href="UserData-BeforePay.html">
+                        <button class="open-button"><span class="glyphicon glyphicon-user"></span> Sign Up</button></a>
+                </ul>
+            </div>
+        </div>
+
+    </div>
+</div>
 
 <?php
 
