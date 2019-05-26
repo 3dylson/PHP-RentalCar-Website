03789d3de671e8d968761788d3cd88bd0335a805
Gelson Alberto Tavares Moreno <a39224@alunos.ipb.pt>
2019-05-26 21:07:21 +0100

    veiculoController update

--- Controllers/VeículoController.php
+++ Controllers/VeículoController.php
@@ -10,12 +10,28 @@ class VeículoController
                 $msg["estado"] = 'Insira o n_Registro';
             } elseif (!isset($_POST['Disponibilidade'])) {
                 $msg["estado"] = 'Insira a disponibilidade';
+            } else {
+                self::CriarVeiclo();
+                $msg["estado"] = 'Criado Com Sucesso!';
             }
-            else {
-                    self::CriarVeiclo();
-                    $msg["estado"] = 'Criado Com Sucesso!';
-                }
-                return $msg["estado"];
-            }
+            return $msg["estado"];
         }
     }
+    static public function criarVeiculo()
+    {
+        $Veiculo = new Veículo('', $_POST['NumeroDeRegistro'], $_POST['Disponibilidade']);
+        $Veiculo->create();
+    }
+    static public function getByDisponibilidade()
+    {
+        Quarto::getByDisponibilidade();
+    }
+    static public function checkDisponibilidade()
+    {
+        return Quarto::checkDisponibilidade();
+    }
+    static public function mostrarQuartosDisponiveis()
+    {
+        Quarto::mostrarQuartosDisponiveis();
+    }
+}
