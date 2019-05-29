8ddea9e7e2e92802850634f202ef5f8fde6f6b38
Gelson Alberto Tavares Moreno <a39224@alunos.ipb.pt>
2019-05-29 23:30:38 +0100

    update signup

--- PL/User/SignUp.php
+++ PL/User/SignUp.php
@@ -9,16 +9,38 @@
     <body class="Definic">
         <section>
             <p class="titile2">Criar uma nova conta</p>
-            <div class="divmain">
+            <div class="form-row">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm">
 
-                <form method="post">
-                    <div><input class="tamanho-inputXL" type="text" placeholder="Nome do Utilizador"></div>
-                    <div><input class="tamanho-inputXL" type="text" placeholder="Nome" name="nome"></div>
-                    <div><input class="tamanho-inputXL" type="text" placeholder="Email"></div>
-                    <div><input class="tamanho-inputXL" type="text" placeholder="Data de Nascimento"></div>
-                    <div><input class="tamanho-inputXL" type="text" placeholder="Password"></div>
-                    <div> <input class="btn btn-primary" type="submit" value="Registar" name="user-signup" ></div>
-                </form>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="validationDefault01">Nome</label>
+                            <input type="text" class="form-control" id="validationDefault01" placeholder="First name" value="Mark" required>
+                            <label for="validationDefault02">Last name</label>
+                            <input type="text" class="form-control" id="validationDefault02" placeholder="Last name" value="Otto" required>
+                            <label for="validationDefaultUsername">Username</label>
+                            <span class="input-group-text" id="inputGroupPrepend2">@</span>
+                            <input type="text" class="form-control" id="validationDefaultUsername" placeholder="Username" aria-describedby="inputGroupPrepend2" required>
+                            <label for="validationDefault03">City</label>
+                            <input type="text" class="form-control" id="validationDefault03" placeholder="City" required>
+                            <label for="validationDefault04">State</label>
+                            <input type="text" class="form-control" id="validationDefault04" placeholder="State" required>
+                            <label for="validationDefault05">Zip</label>
+                            <input type="text" class="form-control" id="validationDefault05" placeholder="Zip" required>
+                            <br>
+                            <button class="btn btn-primary" type="submit">Submit form</button><div>
+                            </div>
+                        </div>
+                        <div class="col-sm">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3">
+
+                </div>
             </div>
         </section>
 
