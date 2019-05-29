60fe945d66bc3b6727a82f14f2fb1fc46ecc35e3
Aritson Lima Melo <a36749@alunos.ipb.pt>
2019-05-30 00:42:58 +0100

    coloquei uns comentadorios neste file.

--- PL/User/DefinicoesConta.php
+++ PL/User/DefinicoesConta.php
@@ -1,13 +1,18 @@
 <section>
     <h1 class="titile">
-        DEFINIÇÕS DA CONTA
+        DEFINIÇÕES DA CONTA
     </h1>
     <div class="divmain">
         <form method="post">
-            <div>  <input type="text" placeholder="Email" ></div>
+            <!--begin display user account information-->
+            <div>  <input type="text" placeholder="Email" name="userEmail"></div>
             <div>  <input type="text" placeholder="Password" name="old_password"> </div>
+            <!--end display user account information-->
+            <!--Criar 1 drop down, botao editar-->
             <div>  <input type="text" placeholder="Nova Password" name="new_password"> </div>
             <div>  <input type="text" placeholder="Confirmar Password" name="repeatNewPass"> </div>
             <div> <input type="submit" value="Atualizar" name="changepass"></div>
+            <!--end of drop down-->
+        </form>
     </div>
 </section>
