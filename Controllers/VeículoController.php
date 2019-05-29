ce4488bff8b91b81a0b94993f107b81d8bb08058
Edylson Manuel Goncalves Frederico <a39221@alunos.ipb.pt>
2019-05-29 11:27:55 +0100

    Veículo Concluído

--- Controllers/VeículoController.php
+++ Controllers/VeículoController.php
@@ -5,13 +5,13 @@ class VeículoController
 {
     static public function Process()
     {
-        if (isset($_POST['idVeiculo'])) {
+        if (isset($_POST['Add'])) {
             if (!isset($_POST['NumeroDeRegistro'])) {
                 $msg["estado"] = 'Insira o n_Registro';
             } elseif (!isset($_POST['Disponibilidade'])) {
                 $msg["estado"] = 'Insira a disponibilidade';
             } else {
-                self::CriarVeiclo();
+                self::criarVeiculo();
                 $msg["estado"] = 'Criado Com Sucesso!';
             }
             return $msg["estado"];
@@ -19,24 +19,23 @@ class VeículoController
     }
     static public function criarVeiculo()
     {
-        $Veiculo = new Veículo('', $_POST['NumeroDeRegistro'], $_POST['Disponibilidade']);
+        $Veiculo = new Veículo('', $_POST['NumeroDeRegistro'], $_POST['Disponibilidade'], '', '', $_POST['Img'] );
         $Veiculo->create();
     }
-    static public function getDisponibilidade()
-    {
-        Veículo::getByDisponibilidade();
+    static public function mostrarVeiculos(){
+        Veículo::mostrarVeiculos();
     }
-    static public function checkDisponibilidade()
-    {
-        return Veículo::checkDisponibilidade();
+    static public function getVeiculoInfo(){
+        return Veículo::getVeiculoInfo();
     }
-    static public function changeOcup()
-    {
-        return Veículo::changeOcup();
+    static public function verificarDisponibilidade(){
+        return Veículo::verificarDisponibilidade();
     }
-    static public function ChangeVeículoFree()
-    {
-        return Veículo::ChangeVeículoFree;
+    static public function changeDisp($q){
+        Veículo::changeDisp($q);
+    }
+    static public function ChangeVeicToFree(){
+        return Veículo::ChangeVeicToFree();
     }
 
 }
