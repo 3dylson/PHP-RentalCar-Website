55c3a167da73581e3114f818bd2bc080cba92ea3
Gelson Alberto Tavares Moreno <a39224@alunos.ipb.pt>
2019-05-30 00:58:14 +0100

    update signup

--- PL/User/SignUp.php
+++ PL/User/SignUp.php
@@ -1,36 +1,36 @@
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>MELITOUR-Sign Up</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-        <link rel="stylesheet" href=".././../CSS/StyleGE.css">
-    </head>
-    <body class="Definic">
+
         <section>
-            <p class="titile2">Criar uma nova conta</p>
+<!--            <p class="titile2">Sign Up</p>-->
             <div class="form-row">
+
                 <div class="container">
+                    <p class="titile2">Sign Up</p>
                     <div class="row">
+
                         <div class="col-sm">
 
                         </div>
+
                         <div class="col-md-6">
-                            <label for="validationDefault01">Nome</label>
-                            <input type="text" class="form-control" id="validationDefault01" placeholder="First name" value="Mark" required>
-                            <label for="validationDefault02">Last name</label>
-                            <input type="text" class="form-control" id="validationDefault02" placeholder="Last name" value="Otto" required>
+
                             <label for="validationDefaultUsername">Username</label>
-                            <span class="input-group-text" id="inputGroupPrepend2">@</span>
-                            <input type="text" class="form-control" id="validationDefaultUsername" placeholder="Username" aria-describedby="inputGroupPrepend2" required>
-                            <label for="validationDefault03">City</label>
-                            <input type="text" class="form-control" id="validationDefault03" placeholder="City" required>
-                            <label for="validationDefault04">State</label>
-                            <input type="text" class="form-control" id="validationDefault04" placeholder="State" required>
-                            <label for="validationDefault05">Zip</label>
-                            <input type="text" class="form-control" id="validationDefault05" placeholder="Zip" required>
+                            <input type="text" class="form-control" id="validationDefaultUsername" placeholder="Gelgatm" name="nome_login" aria-describedby="inputGroupPrepend2" required>
+                            <label for="validationDefault01">Nome</label>
+                            <input type="text" class="form-control" id="validationDefault01" placeholder="ex: Gelson"  required>
+                            <label for="validationDefault02">Email</label>
+                            <input type="text" class="form-control" id="validationDefault02" placeholder="ex: gel@gmail.com"  required>
+                            <label for="validationDefault03">Data de Nascimento</label>
+                            <input type="text" class="form-control" id="validationDefault03" placeholder="ex: 10/14/94" required>
+                            <label for="validationDefault04">NIF</label>
+                            <input type="text" class="form-control" id="validationDefault04" placeholder="ex: 944145235" required>
+                            <label for="inputPassword3" class="col- col-form-label">Password</label>
+                            <input type="password" class="form-control" id="col- col-form-label"  required>
+                            <label for="inputPassword3" class="col- col-form-label">Confirmar Password</label>
+                            <input type="password" class="form-control" id="col- col-form-label"  required>
                             <br>
-                            <button class="btn btn-primary" type="submit">Submit form</button><div>
+                            <input class="btn btn-primary" type="submit" value="Submit" name="user-signup" width="inherit" >
+                            <div>
+
                             </div>
                         </div>
                         <div class="col-sm">
@@ -44,5 +44,4 @@
             </div>
         </section>
 
-    </body>
-</html>
+
