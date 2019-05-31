f687e4d569ab27d65654b20c3f55f9544215ca4c
Gelson Alberto Tavares Moreno <a39224@alunos.ipb.pt>
2019-05-31 23:50:14 +0100

    fiz algumas update de input

--- PL/User/DefinicoesConta.php
+++ PL/User/DefinicoesConta.php
@@ -1,18 +1,35 @@
 <section>
-    <h1 class="titile">
-        DEFINIÇÕES DA CONTA
-    </h1>
-    <div class="divmain">
-        <form method="post">
-            <!--begin display user account information-->
-            <div>  <input type="text" placeholder="Email" name="userEmail"></div>
-            <div>  <input type="password" placeholder="Password" name="old_password"> </div>
-            <!--end display user account information-->
-            <!--Criar 1 drop down, botao editar-->
-            <div>  <input type="text" placeholder="Nova Password" name="new_password"> </div>
-            <div>  <input type="text" placeholder="Confirmar Password" name="repeatNewPass"> </div>
-            <div> <input type="submit" value="Atualizar" name="changepass"></div>
-            <!--end of drop down-->
-        </form>
+    <!--            <p class="titile2">Sign Up</p>-->
+    <div class="form-row">
+
+        <div class="container">
+            <p class="titile2">Definições Da Conta</p>
+            <div class="row">
+                <div class="col-sm">
+                </div>
+                <div class="col-md-6">
+                    <!--begin display user account information-->
+                    <label for="validationDefault02">Email</label>
+                    <input type="text" class="form-control" id="validationDefault02" placeholder="ex: gel@gmail.com" name="userEmail" required>
+                    <label for="inputPassword3" class="col- col-form-label">Password </label>
+                    <input type="password" class="form-control" id="col- col-form-label" name="old_password" required>
+                    <!--end display user account information-->
+                    <!--Criar 1 drop down, botao editar-->
+                    <label for="inputPassword3" class="col- col-form-label">Nova Password</label>
+                    <input type="password" class="form-control" id="col- col-form-label" name="new_password" required>
+                    <label for="inputPassword3" class="col- col-form-label">Confirmar Password</label>
+                    <input type="password" class="form-control" id="col- col-form-label" name="repeatNewPass" required>
+                    <br>
+                    <input class="btn btn-primary" type="submit" value="Atualizar" name="changepass" width="inherit" >
+                    <!--end of drop down-->
+                    <div>
+                    </div>
+                </div>
+                <div class="col-sm">
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-3">
+        </div>
     </div>
 </section>
