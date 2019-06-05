acebfe3438388be50414c3b77f03cc1b8e121a80
Aritson Lima Melo <a36749@alunos.ipb.pt>
2019-06-05 10:48:15 +0100

    Tentei colocar centralizado os conteudos da pagina.

--- PL/DefinicoesConta.php
+++ PL/DefinicoesConta.php
@@ -1,31 +1,36 @@
-<section>
+<div xmlns="http://www.w3.org/1999/html">
     <!--            <p class="titile2">Sign Up</p>-->
+
     <div class="form-row">
 
         <div class="container">
+
             <p class="titile2">Definições Da Conta</p>
+
             <div class="row">
+
                 <div class="col-sm">
-                </div>
-                <div class="col-md-6">
-                    <!--begin display user account information-->
-                    <label for="validationDefault02">Email</label>
-                    <input type="text" class="form-control" id="validationDefault02" placeholder="ex: gel@gmail.com" name="userEmail" required>
-                    <label for="inputPassword3" class="col- col-form-label">Password </label>
-                    <input type="password" class="form-control" id="col- col-form-label" name="old_password" required>
-                    <!--end display user account information-->
-                    <!--Criar 1 drop down, botao editar-->
-                    <label for="inputPassword3" class="col- col-form-label">Nova Password</label>
-                    <input type="password" class="form-control" id="col- col-form-label" name="new_password" required>
-                    <label for="inputPassword3" class="col- col-form-label">Confirmar Password</label>
-                    <input type="password" class="form-control" id="col- col-form-label" name="repeatNewPass" required>
-                    <br>
-                    <input class="btn btn-primary" type="submit" value="Atualizar" name="changepass" width="inherit" >
-                    <!--end of drop down-->
-                    <div>
+
+                    <div class="col-md-6">
+                        <div class="divmain">
+                        <!--begin display user account information-->
+                        <input type="text" class="form-control" id="validationDefault02" placeholder="Email" name="userEmail" required>
+                        </br>
+                        <input type="password" class="form-control" id="col- col-form-label" name="old_password" placeholder="Password" required>
+                        <!--end display user account information-->
+                        <!--Criar 1 drop down, botao editar-->
+                        </br>
+                        <input type="password" class="form-control" id="col- col-form-label"  placeholder="Nova Password" name="new_password" required>
+                        </br>
+                        <input type="password" class="form-control" id="col- col-form-label" placeholder="Confirmar Password" name="repeatNewPass" required>
+                        <br>
+                        <input class="btn btn-primary" type="submit" value="Atualizar" name="changepass" width="inherit" >
+                        <!--end of drop down-->
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
+</div>
 </section>
