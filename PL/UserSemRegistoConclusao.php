ded83b31932c9f339cf8ebe67ec37534dffd62bc
Aritson Lima Melo <a36749@alunos.ipb.pt>
2019-06-07 07:59:46 +0100

    updates.

--- PL/UserSemRegistoConclusao.php
+++ PL/UserSemRegistoConclusao.php
@@ -3,7 +3,35 @@
     <div class="divmain">
 
         <form method="POST">
-            <a id="jasoucliente" href="#"><span class="glyphicon glyphicon-user"></span>&#160Já sou cliente Melitour</a><br/>
+
+            <button class="open-button" onclick="openForm()"><span class="glyphicon glyphicon-log-in"></span>&#160Já sou cliente Melitour</button>
+            <div class="form-popup" id="myForm">
+                <form method="post" class="form-container" >
+                    <h1></h1>
+
+                    <label for="email"><b>Email</b></label>
+                    <br>
+                    <input id="log2" type="email" placeholder="Enter Email" name="userEmail" required>
+
+                    <label for="psw"><b>Password</b></label>
+                    <input id="log" type="password" placeholder="Enter Password" name="signUp_Password" required>
+                    <br>
+                    <button type="submit" name="Login" class="btn">Login</button>
+                    <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
+                </form>
+                <script>
+                    function openForm() {
+                        document.getElementById("myForm").style.display = "block";
+                    }
+
+                    function closeForm() {
+                        document.getElementById("myForm").style.display = "none";
+                    }
+                </script>
+            </div>
+
+
+           <!-- <a id="jasoucliente" href="#"><span class="glyphicon glyphicon-user"></span>&#160Já sou cliente Melitour</a><br/>-->
             <br/><input class="tamanho-inputXL" type="text" name="nome_login" placeholder="Nome de login"> <br/>
             <br/><input class="tamanho-inputXL" type="text" name="userName" placeholder="Nome Completo"> <br/>
             <br/><input class="tamanho-inputXL" type="text" name="userEmail" placeholder="Email"><br/>
