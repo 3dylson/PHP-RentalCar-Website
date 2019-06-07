4135f275e5ec76a6d27188a057f66a7008a745b1
Aritson Lima Melo <a36749@alunos.ipb.pt>
2019-06-07 06:53:35 +0100

    updates.

--- PL/UserSemRegistoConclusao.php
+++ PL/UserSemRegistoConclusao.php
@@ -2,23 +2,17 @@
     <p class="titile2">Os Seus Dados</p>
     <div class="divmain">
 
-        <form action="" method="get">
+        <form method="POST">
             <a id="jasoucliente" href="#"><span class="glyphicon glyphicon-user"></span>&#160Já sou cliente Melitour</a><br/>
-            <div>
-
-                <input class="tamanho-inputXL" type="text" placeholder="Nome Completo">
-            </div>
-            <div>  <input class="tamanho-inputXL" type="text" placeholder="Email"></div>
-            <div>  <input class="tamanho-inputXL" type="text" placeholder="Password"></div>
-            <div><input class="tamanho-inputXL" type="text" placeholder="Confirmar Password"></div>
-
-            <div>  <input class="tamanho-inputXL" type="text" placeholder="NIF"></div>
-
-            <div>
-                <input type="radio" name="exampleRadios" value="option1"> <span class="confirma">Confirmo que os meus dados estão correto
-
-            </div>
-            <div><a href="/index.php?page=PagamentoConclusao"><input class="tamanho-inputXL" type="button" value="Pagar"></a></div>
+            <div> <input class="tamanho-inputXL" type="text" name="nome_login" placeholder="Nome de login"> </div>
+            <div> <input class="tamanho-inputXL" type="text" name="userName" placeholder="Nome Completo"> </div>
+            <div> <input class="tamanho-inputXL" type="text" name="userEmail" placeholder="Email"></div>
+            <div> <input class="tamanho-inputXL" type="password" name="signUp_Password" placeholder="Password"></div>
+            <div> <input class="tamanho-inputXL" type="password" name="signUp_passwordConfirmated" placeholder="Confirmar Password"></div>
+            <div> <input class="tamanho-inputXL" type="text" name="dataNascimento" placeholder="Data de Nascimento (AAAA/MM/DD)"> </div>
+            <div> <input class="tamanho-inputXL" type="text" name="nif" placeholder="NIF"></div>
+           <!-- <div> <input type="radio" name="exampleRadios" value="option1"> <span class="confirma">Confirmo que os meus dados estão correto</div>-->
+            <div><a href="/index.php?page=PagamentoConclusao"><input class="tamanho-inputXL" type="button" name="user-signup" value="Pagar"></a></div>
         </form>
     </div>
 </section>
